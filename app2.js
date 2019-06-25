43de9a244c997dce3e74d22dabde3bec9955119b
abdelk7344 <abdelmonemmoamk7344@hstat.org>
2019-06-25 15:06:08 +0000

    first launch

---
+++ app2.js
@@ -0,0 +1,128 @@
+// Your web app's Firebase configuration
+var apikey = config.API_KEY;
+var firebaseConfig = {
+    apiKey: apikey,
+    authDomain: "darni-b44c7.firebaseapp.com",
+    databaseURL: "https://darni-b44c7.firebaseio.com",
+    projectId: "darni-b44c7",
+    storageBucket: "",
+    messagingSenderId: "408544589504",
+    appId: "1:408544589504:web:7b1883bd19dc7356"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+$(document).ready(function() {
+    showSales()
+});
+
+
+function showSales() {
+    var ref = database.ref("/sales");
+    ref.on("value", gotData, errData)
+    
+}
+
+function gotData(data) {
+    $('.block').remove();
+    var allNums = data.val();
+    var keys = Object.keys(allNums);
+    for (i = 0; keys.length > i; i++) {
+        var k = keys[i]
+        var num = allNums[k]
+        // console.log(num)
+        var sr = ((num[0] + num[1] + num[2] + num[3] + num[4]) / num.length) * 500
+        var pr = ((num[5] + num[6] + num[7] + num[8] + num[9] + num[10] + num[11] + num[12] + num[13] + num[14] + num[15] + num[16] + num[17] + num[18]) / num.length) * 500
+        var er = ((num[19] + num[20] + num[21] + num[22] + num[23] + num[24] + num[25] + num[26] + num[27] + num[28]) / num.length) * 500
+        var qr = ((num[29] + num[30] + num[31] + num[32] + num[33] + num[34] + num[35] + num[36] + num[37] + num[38] + num[39] + num[40] + num[41] + num[42] + num[43]) / num.length) * 500
+        var p = ((num[44] + num[45] + num[46] + num[47] + num[48] + num[49] + num[50] + num[51] + num[52] + num[53] + num[54] + num[55] + num[56] + num[57] + num[58] + num[59]) / num.length) * 500
+        console.log(sr)
+        console.log(pr)
+        console.log(er)
+        console.log(qr)
+        console.log(p)
+
+        $("#container").append('<center><button margin-top:30px;" type="button" class="btn btn-secondary" onclick="reply_click_2($(this).text());showDiv();">' + k + '</button></center><br>');
+        var mother = document.createElement('div');
+        mother.id = 'block';
+        mother.className = 'block';
+        mother.classList.add("progress")
+        document.getElementsByTagName('body')[0].appendChild(mother).style.width = "50%";
+
+
+
+        var name = document.createElement('p');
+        name.className = 'move'
+        name.innerHTML = k;
+        // Now create and append to iDiv
+        var innerDiv1 = document.createElement('div');
+        innerDiv1.className = 'block-2';
+        innerDiv1.classList.add("progressbar")
+        $(innerDiv1).attr('aria-valuenow', sr).css('width', sr);
+        if (sr > 41.61) {
+            innerDiv1.style.backgroundColor = "#D3D3D3"
+        }
+        else {
+            innerDiv1.style.backgroundColor = "#E88D61"
+        }
+        // Now create and append to iDiv
+        var innerDiv2 = document.createElement('div');
+        innerDiv2.className = 'block-2';
+        innerDiv2.classList.add("progress-bar")
+        $(innerDiv2).attr('aria-valuenow', sr).css('width', pr);
+        if (pr > 116.61) {
+            innerDiv2.style.backgroundColor = "#D3D3D3"
+        }
+        else {
+            innerDiv2.style.backgroundColor = "#4AB0CF"
+        }
+        // Now create and append to iDiv
+        var innerDiv3 = document.createElement('div');
+        innerDiv3.className = 'block-2';
+        innerDiv3.classList.add("progress-bar")
+        $(innerDiv3).attr('aria-valuenow', sr).css('width', er);
+        if (er > 83.31) {
+            innerDiv3.style.backgroundColor = "#D3D3D3"
+        }
+        else {
+            innerDiv3.style.backgroundColor = "#00A911"
+        }
+        // Now create and append to iDiv
+        var innerDiv4 = document.createElement('div');
+        innerDiv4.className = 'block-2';
+        innerDiv4.classList.add("progress-bar")
+        $(innerDiv4).attr('aria-valuenow', sr).css('width', qr);
+        if (qr == 125) {
+            innerDiv4.style.backgroundColor = "#D3D3D3"
+        }
+        else {
+            innerDiv4.style.backgroundColor = "#DE66A2"
+        }
+        // Now create and append to iDiv
+        var innerDiv5 = document.createElement('div');
+        innerDiv5.className = 'block-2';
+        innerDiv5.classList.add("progressbar")
+        $(innerDiv5).attr('aria-valuenow', sr).css('width', p);
+        if (p > 133.31) {
+            innerDiv5.style.backgroundColor = "#D3D3D3"
+        }
+        else {
+            innerDiv5.style.backgroundColor = "#4D1370"
+        }
+
+        // The variable iDiv is still good... Just append to it.
+        mother.appendChild(name)
+        mother.appendChild(innerDiv1);
+        mother.appendChild(innerDiv2);
+        mother.appendChild(innerDiv3);
+        mother.appendChild(innerDiv4);
+        mother.appendChild(innerDiv5);
+    }
+}
+
+function errData(err) {
+    console.log(err)
+}
+
---
+++ app5.js
@@ -0,0 +1,227 @@
+var allNums, onesect, k, a, b, currentQ2S, boxId, currentArray, orderId, clicked_id, date, currentDates, allDates, firebase, allQ2s;
+var apikey = config.API_KEY;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: apikey,
+    authDomain: "darni-b44c7.firebaseapp.com",
+    databaseURL: "https://darni-b44c7.firebaseio.com",
+    projectId: "darni-b44c7",
+    storageBucket: "",
+    messagingSenderId: "408544589504",
+    appId: "1:408544589504:web:7b1883bd19dc7356"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Get a reference to the database service
+var database = firebase.database();
+$(window).on("load", function(e) {
+    showSales()
+    var today = new Date();
+    date = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
+    var ref3 = database.ref("/quotetosales")
+    ref3.on("value", gotData3, errData3)
+
+
+});
+
+function addSales() {
+    var newsale = $("#newSaleNum").val();
+    if(newsale.length==0){
+        return
+    }
+    firebase.database().ref("sales/" + newsale).set([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    firebase.database().ref("dates/" + newsale).set([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    firebase.database().ref("quotetosales/" + newsale).set(0)
+    console.log(newsale)
+    showSales()
+    var ref3 = database.ref("/quotetosales")
+    ref3.on("value", gotData3, errData3)
+
+}
+
+function showSales() {
+    var ref = database.ref("/sales");
+    var ref2 = database.ref("/dates")
+    ref2.on("value", gotData2, errData2)
+    ref.on("value", gotData, errData)
+}
+
+function showDiv() {
+    var x = document.getElementById("checklist1");
+    var checked = 0;
+    for (i = 0; currentArray.length > i; i++) {
+        checked += currentArray[i]
+    }
+    $('#progBar').attr('value', (checked / 60) * 100);
+}
+
+function reply_click(orginal_clicked) {
+    var ref3 = database.ref("/quotetosales")
+    ref3.on("value", gotData3, errData3)
+    var checked = 0;
+    boxId = orginal_clicked;
+
+    if (($(document.getElementById(boxId)).prop("checked") == true)) {
+        currentArray[boxId] = 1
+        if (boxId == "5") {
+            assignLevel()
+        }
+        currentDates[boxId] = date;
+        firebase.database().ref("sales/" + clicked_id).set(currentArray)
+        firebase.database().ref("dates/" + clicked_id).set(currentDates)
+        var ref3 = database.ref("/quotetosales")
+        ref3.on("value", gotData3, errData3)
+        if (boxId == "15") {
+            getSalesNumber()
+        }
+        for (i = 0; currentArray.length > i; i++) {
+            checked += currentArray[i]
+            if (currentDates[i] !== 0) {
+                document.getElementById(60 + i).innerHTML = currentDates[i];
+                document.getElementById(60 + i).style.fontStyle = "italic";
+            }
+        }
+        $(document.getElementById('progBar')).attr('value', (checked / 60) * 100);
+        console.log(checked)
+        if (checked == 60) {
+            console.log("delete")
+            var confirm = prompt("Are you DONE with this order?! Type yes or no");
+            if (confirm.toLowerCase() == "yes") {
+                firebase.database().ref("sales/" + clicked_id).remove()
+                var ref3 = database.ref("/quotetosales")
+                ref3.on("value", gotData3, errData3)
+                window.location.replace(window.location.pathname + window.location.search + window.location.hash);
+            }
+            else {
+                alert("You Need To Uncheck What Has Not Yet Been Completed")
+
+            }
+
+        }
+    }
+    else if (($(document.getElementById(boxId)).prop("checked") == false)) {
+
+        currentArray[boxId] = 0
+        currentDates[boxId] = 0;
+        firebase.database().ref("sales/" + clicked_id).set(currentArray)
+        firebase.database().ref("dates/" + clicked_id).set(currentDates)
+        var ref3 = database.ref("/quotetosales")
+        ref3.on("value", gotData3, errData3)
+        for (i = 0; currentArray.length > i; i++) {
+            checked += currentArray[i];
+        }
+        $('#progBar').attr('value', (checked / 60) * 100);
+    }
+}
+
+function reply_click_2(clicked_id_2) {
+    $("#header2").empty()
+    var ref3 = database.ref("/quotetosales")
+    ref3.on("value", gotData3, errData3)
+    clicked_id = clicked_id_2;
+    currentArray = allNums[clicked_id];
+    currentDates = allDates[clicked_id]
+    currentQ2S = allQ2s[clicked_id]
+    if (currentQ2S !== 0) {
+        document.getElementById(clicked_id).innerHTML = currentQ2S;
+        $("#header2").text('You are currently viewing quote order ' + clicked_id);
+        $("#header").text('You are currently viewing sales order ' + currentQ2S);
+    }
+    else {
+        $("#header").text('You are currently viewing quote order ' + clicked_id);
+    }
+    for (var i = 0; i < currentArray.length; i++) {
+        document.getElementById(i).checked = currentArray[i]
+        if (currentDates[i] !== 0) {
+            document.getElementById(60 + i).innerHTML = currentDates[i];
+            document.getElementById(60 + i).style.fontStyle = "italic";
+        }
+        else {
+            document.getElementById(60 + i).innerHTML = "";
+        }
+    }
+}
+
+function gotData(data) {
+    $('#container').empty();
+    allNums = data.val();
+    var keys = Object.keys(allNums);
+    for (var i = 0; keys.length > i; i++) {
+        k = keys[i]
+        var num = allNums[k]
+        $("#container").append('<center><button style="float: left;  margin-left: 30px; margin-top:30px;color:orange;" id="' + k + '" type="button" class="btn btn-secondary" onclick="reply_click_2(this.id);showDiv();">' + k + '</button></center>');
+
+    }
+}
+
+function errData(err) {
+    console.log(err)
+}
+
+function gotData2(data) {
+    allDates = data.val();
+    var keys = Object.keys(allDates);
+    for (var i = 0; keys.length > i; i++) {
+        a = keys[i]
+        var cdate = allDates[a]
+    }
+}
+
+function errData2(err) {
+    console.log(err)
+}
+
+function gotData3(data) {
+    allQ2s = data.val()
+    var keys = Object.keys(allQ2s);
+    for (var i = 0; keys.length > i; i++) {
+        b = keys[i]
+        var Q2S = allQ2s[b]
+        if (Q2S !== 0) {
+            document.getElementById(b).innerHTML = Q2S;
+            document.getElementById(b).style.color = "white";
+        }
+        else {
+            document.getElementById(b).innerHTML = b;
+            document.getElementById(b).style.color = "orange";
+        }
+    }
+
+}
+
+function errData3(err) {
+    console.log(err)
+}
+
+function assignLevel() {
+    var level = prompt("Is this job level 1? Please enter yes or no.");
+    if (level.toLowerCase() == "yes") {
+        for (var i = 5; i < 44; i++) {
+            currentArray[i] = 1
+            currentDates[i] = "N/A"
+            document.getElementById(i).checked = currentArray[i]
+        }
+    }
+    else {
+        alert("I did not understand what you have written down. Please try again.")
+        currentArray[5] = 0
+    }
+}
+
+function getSalesNumber() {
+    $("#header2").empty()
+    $("#header").empty()
+    var quotetosales = prompt("Please Enter the Sales Number");
+    if (quotetosales.length<25) {
+        firebase.database().ref("quotetosales/" + clicked_id).set(quotetosales)
+        document.getElementById(clicked_id).innerHTML = quotetosales;
+        document.getElementById(clicked_id).style.color = "white";
+        $("#header2").text('You are currently viewing quote order ' + clicked_id);
+        $("#header").text('You are currently viewing sales order ' + quotetosales);
+    }
+    else {
+        alert("You did not enter 5 characters!")
+    }
+
+}
